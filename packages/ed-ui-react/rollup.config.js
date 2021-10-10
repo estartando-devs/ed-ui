208a0e7d9a37bc8fcb0545557961fd536d5142f7
jonatapinto <jonatastdb_jp@hotmail.com>
2021-10-10 01:03:55 -0300

    fix

--- packages/ed-ui-react/rollup.config.js
+++ packages/ed-ui-react/rollup.config.js
@@ -3,8 +3,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
 import postcss from "rollup-plugin-postcss";
-
-// rollup-plugin-peer-deps-external @rollup/plugin-node-resolve @rollup/plugin-commonjs rollup-plugin-typescript2 rollup-plugin-postcss
+import { babel } from '@rollup/plugin-babel'
 
 const packageJson = require("./package.json");
 
@@ -14,21 +13,64 @@ export default {
     {
       file: packageJson.main,
       format: "cjs",
-      sourcemap: true
+      sourcemap: true,
+      freeze: false,
+      esModule: true,
     },
     {
       file: packageJson.module,
-      format: "esm",
-      sourcemap: true
+      format: "es",
+      sourcemap: true,
+      freeze: false,
+      esModule: true,
     }
   ],
   plugins: [
     peerDepsExternal(),
     resolve(),
     commonjs(),
-    typescript({ useTsconfigDeclarationDir: true }),
+    typescript({ 
+      useTsconfigDeclarationDir: true,
+      tsconfigDefaults: {
+        exclude: [
+          // all TS test files, regardless whether co-located or in test/ etc
+          '**/*.spec.ts',
+          '**/*.test.ts',
+          '**/*.spec.tsx',
+          '**/*.test.tsx',
+        ],
+        compilerOptions: {
+          sourceMap: true,
+          declaration: true,
+          jsx: 'react',
+        },
+      },
+      tsconfigOverride: {
+        compilerOptions: {
+          // TS -> esnext, then leave the rest to babel-preset-env
+          target: 'esnext',
+        },
+      },
+      typescript: require('typescript'),
+    }),
     postcss({
-        extensions: ['.css']
-    })
+      extensions: ['.css']
+    }),
+    babel({
+      extensions: ['.ts', '.tsx'],
+      babelHelpers: 'runtime',
+      include: 'src/**/*',
+      exclude: 'node_modules/**',
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            modules: false,
+            loose: true,
+          },
+        ],
+      ],
+      plugins: ['@babel/plugin-transform-runtime'],
+    }),
   ]
 };
