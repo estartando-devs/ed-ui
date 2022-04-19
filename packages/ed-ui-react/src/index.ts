ab8890711707d199f4e484b18a2dd1f4ba95ca07
Ramon <ramon1234rxm@gmail.com>
2022-04-19 08:36:59 -0300

    chore: remove old packages and configs

--- packages/ed-ui-react/src/index.ts
+++
@@ -1,2 +0,0 @@
-export { Button } from "./Button"
-export { EDUIProvider, defaultTheme, GlobalStyles} from "./styles"
--- packages/ed-ui-types/src/style/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./interfaces"
-export { ITheme } from "./ITheme"
