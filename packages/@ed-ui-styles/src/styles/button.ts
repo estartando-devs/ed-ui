57f6c26b49993a98cc793b97c7fb4fec982b4c33
Ramon <ramon1234rxm@gmail.com>
2022-04-19 08:36:44 -0300

    feat: create a styles packages to agnostic styles

---
+++ packages/@ed-ui-styles/src/styles/button.ts
@@ -0,0 +1,15 @@
+import { css } from '@stitches/core';
+
+const buttonCommonStyles = css({
+  fontSize: '13px',
+  padding: '10px 15px',
+  border: 'none',
+  outline: 'none',
+  background: '#81CAA8',
+  boxShadow: '0px 4px 31px rgba(255, 255, 255, 0.2)',
+  borderRadius: '4px',
+  color: '#fff',
+  cursor: 'pointer',
+});
+
+export { buttonCommonStyles };
