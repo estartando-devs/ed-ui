bb28a3431ab5249ffb0d5521cd470e6e0f069329
Ramon Xavier <ramonxavier@WP090501.local>
2023-01-03 16:41:01 -0300

    fix: cleanup project

--- packages/react-components/src/components/Button/types/ButtonProps.ts
+++
@@ -1,9 +0,0 @@
-import type * as Stitches from '@stitches/react';
-
-export type ButtonProps = {
-  children: React.ReactNode;
-  variant?: 'primary' | 'secondary' | 'tertiary';
-  size?: 'small' | 'medium' | 'large';
-  isLoading?: boolean;
-  css?: Stitches.CSS;
-} & React.HTMLAttributes<HTMLButtonElement>;
